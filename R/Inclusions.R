f57419ccc4b66c7f8a048637e6b116e87b0edab5
MathildeLV <mathilde.levu@iem.uzh.ch>
2022-08-09 10:57:32 +0200

    exchanging the frist two exclusion steps and changing exclusion for BW and GA- stillborn def

--- R/Inclusions.R
+++ R/Inclusions.R
@@ -1,30 +1,32 @@
 ##Inclusion steps
-
-# GA present: from 2007 only: made as a 1st step with R file "bevn_from_2007.R"
-# exclusion of missing 
-  table(bevn_eco$GA_weeks, useNA = "always")
-  #removal of 138581 entries with missing GA
-  bevn_eco_in0 <- bevn_eco %>%
-  filter(!is.na(GA_weeks))
-  dim(bevn_eco)-dim(bevn_eco_in0)
-  dim(bevn_eco_in0)
-
+# Data from 2007 only: made as a 1st step with R file "bevn_from_2007.R" to exclude absent GA in the previous years
 
 # Births taking place inside of Switzerland
-  table(bevn_eco$country_of_birth_cat1, useNA = "always")
-  # removal of 124074  (outside of Switz) and 1 (NA) entries
-  
-  bevn_eco_in1 <- bevn_eco_in0 %>%
+table(bevn_eco$country_of_birth_cat1, useNA = "always")
+# removal of 124074  (outside of Switz) and 1 (NA) entries
+
+bevn_eco_in0 <- bevn_eco %>%
   filter(country_of_birth_cat1=="Switzerland")
-  table(bevn_eco_in1$country_of_birth_cat1)
-  
+table(bevn_eco_in0$country_of_birth_cat1)
+
+dim(bevn_eco)-dim(bevn_eco_in0)
+dim(bevn_eco_in0)
+#removal of 124075 entries. new nb entries: 1202203
+
+#GA missing
+  table(bevn_eco$GA_weeks, useNA = "always")
+  #removal of 138581 entries with missing GA
+  bevn_eco_in1 <- bevn_eco_in0 %>%
+  filter(!is.na(GA_weeks))
   dim(bevn_eco_in0)-dim(bevn_eco_in1)
   dim(bevn_eco_in1)
-  #removal of 12 new entries. new nb entries: 1155185
+  table(bevn_eco_in1$GA_weeks, useNA = "always")
+  
+#removal of 14518 entries. new nb of entries: 1187685
+
   
 #Place of residence of the mother: exclude mothers domiciled abroad / resident_status
-  table(bevn_eco$resident_status, useNA = "always")
-  round(prop.table(table(bevn_eco$resident_status, useNA="always"))*100,2)
++  round(prop.table(table(bevn_eco$resident_status, useNA="always"))*100,2)
   #we will exclude 158606 (status resident=9, domiciled abroad or status= 2 or 4 = non permanent resident or short stay) and no NA
   
    bevn_eco_in2 <- bevn_eco_in1 %>%
@@ -51,8 +53,10 @@
    
 
 # Exclude entries with BW<500 and GA<22 weeks, exclude missing
-   table(bevn_eco$BW_cat2, bevn_eco$GA_weeks_cat2, useNA="always")
-   #we will exclude 666  entries and (13+14497=14510)  NA
+   table(bevn_eco$BW_cat2, useNA="always")
+   table(bevn_eco$GA_weeks_cat2, useNA="always")
+   
+   #we will exclude 752  entries with GA<22 weeks, 2407 with BW < 500g (13+14497=14510)  NA
  
   #check if missing birthweights are for births outside of Switz
    table(bevn_eco$BW_cat2, bevn_eco$country_of_birth_cat1, useNA = "always")
@@ -60,16 +64,16 @@
    # (124062) 99.78% of missing BW happened outside of Switz, 0.22%(269) happened inside Switzerland
    
    bevn_eco_in4 <- bevn_eco_in3 %>%
-     filter(GA_weeks >22 & BW >= 500 | GA_weeks > 22 & BW <500 | GA_weeks<22 & BW > 500) %>%
+     filter(GA_weeks >22 & BW > 500) %>%
      filter(!is.na(BW)) 
 
    dim(bevn_eco_in3)-dim(bevn_eco_in4)
    dim(bevn_eco_in4)
-   #Exclusion of 853 additional entries. New nb entries: 1108246       
+   #Exclusion of 2180 additional entries. New nb entries: 1106919       
    
 # BL between 20 and 65cm only, exclude missing 
    table(bevn_eco$BL_cat, useNA="always")
-   # We will exclude 124295  NAs and 177 BL<20cm
+   # We will exclude 124295  NAs and 177 BL<20cm, and no BL>60cm
    
    #check if missing BL are for births outside of Switz
    table(bevn_eco$BL_cat, bevn_eco$country_of_birth_cat1, useNA = "always")
@@ -83,7 +87,7 @@
    
    dim(bevn_eco_in4)-dim(bevn_eco_in5)
    dim(bevn_eco_in5)
-   #Exclusion of 185 additional entries. New nb entries: 1108061            
+   #Exclusion of 128 additional entries. New nb entries: 1106679            
    
 # Maternal age: exclude missing and exclude age > 50 (to discuss w/ Kaspar)
    table(bevn_eco$mat_age, useNA="always")
@@ -94,7 +98,7 @@
    
    dim(bevn_eco_in5)-dim(bevn_eco_in6)
    dim(bevn_eco_in6)
-   #Exclusion of 112 additional entries
+   #Exclusion of 112 additional entries and no missing NA
    
 # Live births only
    table(bevn_eco$stillbirth, useNA="always")
@@ -106,4 +110,4 @@
    
   dim(bevn_eco_in6)-dim(bevn_eco_in7)
    dim(bevn_eco_in7)
-  #Exclusion of 4134  additional entries. New nb entries: 1107995
+  #Exclusion of 3327  additional entries. New nb entries: 1103352
