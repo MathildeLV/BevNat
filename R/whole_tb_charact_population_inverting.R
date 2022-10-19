5b5c6aed5c961c3eb6f6c173841706f3782eb397
MathildeLV <mathilde.levu@iem.uzh.ch>
2022-10-19 16:13:53 +0200

    making tables for neonatal characteristics inverted(years as col)

--- R/whole_tb_charact_population_inverting.R
+++ R/whole_tb_charact_population_inverting.R
@@ -175,3 +175,81 @@ mat_in_inv_7 %>%
 
 # Neonatal characteristics
 ## At inclusion : in
+neo_in_inv <- t(formatted0)
+rownames(neo_in_inv) <- colnames(formatted0)
+colnames(neo_in_inv) <- rownames(formatted0)
+neo_in_inv
+
+#removing the first row that is useless, to have fir row (and col names) as birthyear
+colnames(neo_in_inv) <- neo_in_inv[1,]
+neo_in_inv <- neo_in_inv[-1, ] 
+neo_in_inv
+neo_in_inv <- as_tibble(neo_in_inv)
+neo_in_inv
+
+neo_in_inv <- add_column(neo_in_inv, .before = 1,
+                           name = c("n", "Birthweight (g), mean ± sd", "Gestational age (weeks), mean ± sd", 
+                                    "Female (%)", "Male (%)", "Stillbirth (%)", "Preterm birth (%)", "Low birthweight (%)", "Macrosomia (%)")
+)
+
+neo_in_inv
+
+ neo_in_inv %>%
+  gt() %>%
+  tab_header(title="Neonatal characteristics at inclusion") %>%
+    cols_label(name = "Birthyear") %>%
+  gtsave(here("output/tables", "neonatal_characteristics_in_inverted.html"))
+neo_in_inv
+
+
+## In dataset 6
+neo_in6_inv <- t(formatted2)
+rownames(neo_in6_inv) <- colnames(formatted2)
+colnames(neo_in6_inv) <- rownames(formatted2)
+neo_in6_inv
+
+#removing the first row that is useless, to have fir row (and col names) as birthyear
+colnames(neo_in6_inv) <- neo_in6_inv[1,]
+neo_in6_inv <- neo_in6_inv[-1, ] 
+neo_in6_inv
+neo_in6_inv <- as_tibble(neo_in6_inv)
+neo_in6_inv
+
+neo_in6_inv <- add_column(neo_in6_inv, .before = 1,
+                         name = c("n", "Birthweight (g), mean ± sd", "Gestational age (weeks), mean ± sd", 
+                                  "Female (%)", "Male (%)", "Stillbirth (%)", "Preterm birth (%)", "Low birthweight (%)", "Macrosomia (%)")
+)
+neo_in6_inv
+
+neo_in6_inv %>%
+  gt() %>%
+  tab_header(title="Neonatal characteristics in dataset 6") %>%
+  cols_label(name = "Birthyear") %>%
+  gtsave(here("output/tables", "neonatal_characteristics_in6_inverted.html"))
+neo_in6_inv
+
+## In dataset 7
+neo_in7_inv <- t(formatted1)
+rownames(neo_in7_inv) <- colnames(formatted1)
+colnames(v) <- rownames(formatted1)
+neo_in7_inv
+
+#removing the first row that is useless, to have fir row (and col names) as birthyear
+colnames(neo_in7_inv) <- neo_in7_inv[1,]
+neo_in7_inv <- neo_in7_inv[-1, ] 
+neo_in7_inv
+neo_in7_inv <- as_tibble(neo_in7_inv)
+neo_in7_inv
+
+neo_in7_inv <- add_column(neo_in7_inv, .before = 1,
+                          name = c("n", "Birthweight (g), mean ± sd", "Gestational age (weeks), mean ± sd", 
+                                   "Female (%)", "Male (%)", "Preterm birth (%)", "Low birthweight (%)", "Macrosomia (%)")
+)
+neo_in7_inv
+
+neo_in7_inv %>%
+  gt() %>%
+  tab_header(title="Neonatal characteristics in dataset 6") %>%
+  cols_label(name = "Birthyear") %>%
+  gtsave(here("output/tables", "neonatal_characteristics_in7_inverted.html"))
+neo_in7_inv
