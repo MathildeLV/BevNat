b24516c1c35387816672f8f388ff5fe06279246f
MathildeLV <mathilde.levu@iem.uzh.ch>
2023-03-08 18:09:46 +0100

    trimester effect variables for the economic crisis and the heatwaves

--- R/dummyvariables_crisis.R
+++ R/dummyvariables_crisis.R
@@ -14,6 +14,42 @@ table(bevn_eco$birthmonth, bevn_eco$HW)
 table(bevn_eco$birthmonth, bevn_eco$birthyear, bevn_eco$HW)
 
 
+# Exposure during one of the pregnancy trimesters
+## Exposure during first trimester (heatwave 2015 or 2018, independently)
+  bevn_eco <- bevn_eco %>%
+  dplyr::mutate(HW_first_trimester=ifelse((month_1=="2015-07-01" | month_1=="2018-08-01"| 
+                                          month_2=="2015-07-01" |month_2=="2018-08-01"| 
+                                          month_3=="2015-07-01" |month_3=="2018-08-01") 
+                                             , '1',
+                                             0),
+                HW_first_trimester=as.factor(HW_first_trimester)) %>%
+  dplyr::mutate(HW_first_trimester= replace(HW_first_trimester,
+                                               (is.na(HW_first_trimester)), 0))
+
+## Exposure during second trimester (heatwave 2015 or 2018, independently)
+  bevn_eco <- bevn_eco %>%
+  dplyr::mutate(HW_second_trimester=ifelse((month_4=="2015-07-01" | month_4=="2018-08-01"| 
+                                              month_5=="2015-07-01" |month_5=="2018-08-01"| 
+                                              month_6=="2015-07-01" |month_6=="2018-08-01") 
+                                           , '1',
+                                           0),
+                HW_second_trimester=as.factor(HW_second_trimester)) %>%
+  dplyr::mutate(HW_second_trimester= replace(HW_second_trimester,
+                                                (is.na(HW_second_trimester)), 0))
+
+## Exposure during third trimester (heatwave 2015 or 2018, independently) - up to delivery
+  bevn_eco <- bevn_eco %>%
+  dplyr::mutate(HW_third_trimester=ifelse((month_7=="2015-07-01" | month_7=="2018-08-01"| 
+                                              month_8=="2015-07-01" |month_8=="2018-08-01"| 
+                                              month_9=="2015-07-01" |month_9=="2018-08-01" |
+                                              month_10=="2015-07-01" |month_10=="2018-08-01"|
+                                              month_11=="2015-07-01" |month_11=="2018-08-01") 
+                                             , '1',
+                                             0),
+                HW_third_trimester=as.factor(HW_third_trimester)) %>%
+  dplyr::mutate(HW_third_trimester= replace(HW_third_trimester,
+                                               (is.na(HW_third_trimester)), 0))
+
 ########################################## Economic crises ###############################################################
 ## Great recession: Q4 2008 and Q1 2009
 bevn_eco <- bevn_eco %>%
@@ -24,6 +60,55 @@ bevn_eco <- bevn_eco %>%
                 GR=as.factor(GR))
 table(bevn_eco$birthmonth, bevn_eco$birthyear, bevn_eco$GR)
 
+# Exposure during one of the pregnancy trimesters
+## Exposure during first trimester
+bevn_eco <- bevn_eco %>%
+  dplyr::mutate(GR_first_trimester=ifelse((month_1=="2008-10-01" | month_1=="2008-11-01"| month_1=="2008-12-01" |
+                                          month_1=="2009-01-01" | month_1=="2009-02-01" | month_1=="2009-03-01" |
+                                          month_2=="2008-10-01" | month_2=="2008-11-01"| month_2=="2008-12-01" |
+                                          month_2=="2009-01-01" | month_2=="2009-02-01" | month_2=="2009-03-01"|
+                                          month_3=="2008-10-01" | month_3=="2008-11-01"| month_3=="2008-12-01" |
+                                          month_3=="2009-01-01" | month_3=="2009-02-01" | month_3=="2009-03-01"
+                                          ) 
+                                          , '1',
+                                          0),
+                GR_first_trimester=as.factor(GR_first_trimester)) %>%
+  dplyr::mutate(GR_first_trimester= replace(GR_first_trimester,
+                                            (is.na(GR_first_trimester)), 0))
+
+## Exposure during second trimester
+bevn_eco <- bevn_eco %>%
+  dplyr::mutate(GR_second_trimester=ifelse((month_4=="2008-10-01" | month_4=="2008-11-01"| month_4=="2008-12-01" |
+                                              month_4=="2009-01-01" | month_4=="2009-02-01" | month_4=="2009-03-01" |
+                                              month_5=="2008-10-01" | month_5=="2008-11-01"| month_5=="2008-12-01" |
+                                              month_5=="2009-01-01" | month_5=="2009-02-01" | month_5=="2009-03-01"|
+                                              month_6=="2008-10-01" | month_6=="2008-11-01"| month_6=="2008-12-01" |
+                                              month_6=="2009-01-01" | month_6=="2009-02-01" | month_6=="2009-03-01"
+                                          ) 
+                                           , '1',
+                                           0),
+                GR_second_trimester=as.factor(GR_second_trimester)) %>%
+  dplyr::mutate(GR_second_trimester= replace(GR_second_trimester,
+                                             (is.na(GR_second_trimester)), 0))
+
+## Exposure during third trimester - up to delivery
+bevn_eco <- bevn_eco %>%
+  dplyr::mutate(GR_third_trimester=ifelse((month_7=="2008-10-01" | month_7=="2008-11-01"| month_7=="2008-12-01" |
+                                             month_7=="2009-01-01" | month_7=="2009-02-01" | month_7=="2009-03-01" |
+                                             month_8=="2008-10-01" | month_8=="2008-11-01"| month_8=="2008-12-01" |
+                                             month_8=="2009-01-01" | month_8=="2009-02-01" | month_8=="2009-03-01"|
+                                             month_9=="2008-10-01" | month_9=="2008-11-01"| month_9=="2008-12-01" |
+                                             month_9=="2009-01-01" | month_9=="2009-02-01" | month_9=="2009-03-01" |
+                                             month_10=="2009-01-01" | month_10=="2009-02-01" | month_10=="2009-03-01" |
+                                             month_10=="2009-01-01" | month_10=="2009-02-01" | month_10=="2009-03-01" |
+                                             month_11=="2009-01-01" | month_11=="2009-02-01" | month_11=="2009-03-01" |
+                                             month_11=="2009-01-01" | month_11=="2009-02-01" | month_11=="2009-03-01" 
+                                          ) 
+                                          , '1',
+                                          0),
+                GR_third_trimester=as.factor(GR_third_trimester)) %>%
+  dplyr::mutate(GR_third_trimester= replace(GR_third_trimester,
+                                            (is.na(GR_third_trimester)), 0))
 
 ########################################## COVID-19 ###############################################################
 # Delivery date during COVID19
@@ -65,8 +150,6 @@ table(bevn_eco$birthmonth, bevn_eco$birthyear, bevn_eco$COVID_second_wave)
       dplyr::mutate(COVID_second_trimester= replace(COVID_second_trimester,
                                                     (is.na(COVID_second_trimester)), 0))
     
-                          
-
   ## Exposure during third trimester ((wave 1 or wave 2, independently) - up to delivery
   bevn_eco <- bevn_eco %>%
   dplyr::mutate(COVID_third_trimester=ifelse((month_7=="2020-03-01" |month_7=="2020-04-01" | month_7=="2020-10-01" | month_7=="2020-11-01" | month_7=="2020-12-01" | 
