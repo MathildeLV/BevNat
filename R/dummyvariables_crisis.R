410f00c275dcaaebe75aa31692ee8050ffd46e26
MathildeLV <mathilde.levu@iem.uzh.ch>
2023-03-27 13:45:46 +0200

    correcting error line 96-106

--- R/dummyvariables_crisis.R
+++ R/dummyvariables_crisis.R
@@ -15,7 +15,7 @@ table(bevn_eco$birthmonth, bevn_eco$birthyear, bevn_eco$HW)
 
 
 # Exposure during one of the pregnancy trimesters
-## Exposure during first trimester (heatwave 2015 or 2018, independently)
+  ## Exposure during first trimester (heatwave 2015 or 2018, independently)
   bevn_eco <- bevn_eco %>%
   dplyr::mutate(HW_first_trimester=ifelse((month_1=="2015-07-01" | month_1=="2018-08-01"| 
                                           month_2=="2015-07-01" |month_2=="2018-08-01"| 
@@ -26,7 +26,7 @@ table(bevn_eco$birthmonth, bevn_eco$birthyear, bevn_eco$HW)
   dplyr::mutate(HW_first_trimester= replace(HW_first_trimester,
                                                (is.na(HW_first_trimester)), 0))
 
-## Exposure during second trimester (heatwave 2015 or 2018, independently)
+  ## Exposure during second trimester (heatwave 2015 or 2018, independently)
   bevn_eco <- bevn_eco %>%
   dplyr::mutate(HW_second_trimester=ifelse((month_4=="2015-07-01" | month_4=="2018-08-01"| 
                                               month_5=="2015-07-01" |month_5=="2018-08-01"| 
@@ -37,7 +37,7 @@ table(bevn_eco$birthmonth, bevn_eco$birthyear, bevn_eco$HW)
   dplyr::mutate(HW_second_trimester= replace(HW_second_trimester,
                                                 (is.na(HW_second_trimester)), 0))
 
-## Exposure during third trimester (heatwave 2015 or 2018, independently) - up to delivery
+  ## Exposure during third trimester (heatwave 2015 or 2018, independently) - up to delivery
   bevn_eco <- bevn_eco %>%
   dplyr::mutate(HW_third_trimester=ifelse((month_7=="2015-07-01" | month_7=="2018-08-01"| 
                                               month_8=="2015-07-01" |month_8=="2018-08-01"| 
@@ -61,8 +61,8 @@ bevn_eco <- bevn_eco %>%
 table(bevn_eco$birthmonth, bevn_eco$birthyear, bevn_eco$GR)
 
 # Exposure during one of the pregnancy trimesters
-## Exposure during first trimester
-bevn_eco <- bevn_eco %>%
+  ## Exposure during first trimester
+  bevn_eco <- bevn_eco %>%
   dplyr::mutate(GR_first_trimester=ifelse((month_1=="2008-10-01" | month_1=="2008-11-01"| month_1=="2008-12-01" |
                                           month_1=="2009-01-01" | month_1=="2009-02-01" | month_1=="2009-03-01" |
                                           month_2=="2008-10-01" | month_2=="2008-11-01"| month_2=="2008-12-01" |
@@ -76,8 +76,8 @@ bevn_eco <- bevn_eco %>%
   dplyr::mutate(GR_first_trimester= replace(GR_first_trimester,
                                             (is.na(GR_first_trimester)), 0))
 
-## Exposure during second trimester
-bevn_eco <- bevn_eco %>%
+  ## Exposure during second trimester
+  bevn_eco <- bevn_eco %>%
   dplyr::mutate(GR_second_trimester=ifelse((month_4=="2008-10-01" | month_4=="2008-11-01"| month_4=="2008-12-01" |
                                               month_4=="2009-01-01" | month_4=="2009-02-01" | month_4=="2009-03-01" |
                                               month_5=="2008-10-01" | month_5=="2008-11-01"| month_5=="2008-12-01" |
@@ -91,17 +91,17 @@ bevn_eco <- bevn_eco %>%
   dplyr::mutate(GR_second_trimester= replace(GR_second_trimester,
                                              (is.na(GR_second_trimester)), 0))
 
-## Exposure during third trimester - up to delivery
-bevn_eco <- bevn_eco %>%
+  ## Exposure during third trimester - up to delivery
+  bevn_eco <- bevn_eco %>%
   dplyr::mutate(GR_third_trimester=ifelse((month_7=="2008-10-01" | month_7=="2008-11-01"| month_7=="2008-12-01" |
                                              month_7=="2009-01-01" | month_7=="2009-02-01" | month_7=="2009-03-01" |
                                              month_8=="2008-10-01" | month_8=="2008-11-01"| month_8=="2008-12-01" |
                                              month_8=="2009-01-01" | month_8=="2009-02-01" | month_8=="2009-03-01"|
                                              month_9=="2008-10-01" | month_9=="2008-11-01"| month_9=="2008-12-01" |
                                              month_9=="2009-01-01" | month_9=="2009-02-01" | month_9=="2009-03-01" |
+                                             month_10=="2008-10-01" | month_10=="2008-11-01" | month_10=="2008-12-01" |
                                              month_10=="2009-01-01" | month_10=="2009-02-01" | month_10=="2009-03-01" |
-                                             month_10=="2009-01-01" | month_10=="2009-02-01" | month_10=="2009-03-01" |
-                                             month_11=="2009-01-01" | month_11=="2009-02-01" | month_11=="2009-03-01" |
+                                             month_11=="2008-10-01" | month_11=="2008-11-01" | month_11=="2008-12-01" |
                                              month_11=="2009-01-01" | month_11=="2009-02-01" | month_11=="2009-03-01" 
                                           ) 
                                           , '1',
