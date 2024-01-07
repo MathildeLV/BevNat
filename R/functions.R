f2a089ca2d6c511a9c11a9523d8e288ce67fc004
MathildeLV <mathilde.levu@iem.uzh.ch>
2024-01-07 16:03:06 +0100

    removing useless functions adding function to add pvalue to the tables of estimates of GAM models

--- R/functions.R
+++ R/functions.R
@@ -2,7 +2,6 @@
 # Model with a continuous outcome (ex: birthweight)
 GAM_plot_function_continuous <- function(model, var_nr) {
   plot <- plot(model, select=var_nr)
-  
   plot_x <- plot[[var_nr]]$x 
   plot_fit <- plot[[var_nr]]$fit+coef(model)[1]
   plot_CIl<- plot_fit-1.96*plot[[var_nr]]$se
@@ -10,7 +9,6 @@ GAM_plot_function_continuous <- function(model, var_nr) {
   plot_data <-  data.frame(x=plot_x, fit=plot_fit,CIl=plot_CIl,CIu=plot_CIu)
 }
 
-####################
 # Model with a binary outcome (ex: PTB)
 GAM_plot_function_binary <- function(model, var_nr) {
   plot <- plot(model, select=var_nr)
@@ -21,14 +19,6 @@ GAM_plot_function_binary <- function(model, var_nr) {
   plot_data <-  data.frame(x=plot_x, fit=plogis(plot_fit),CIl=plogis(plot_CIl),CIu=plogis(plot_CIu))
 }
 
-GAM_plot_function_binarybis <- function(model, var_nr) {
-  plot <- plot(model, select=var_nr, trans = plogis)
-  plot_x <- plot[[var_nr]]$x 
-  plot_fit <- ((plot[[var_nr]]$fit))
-  plot_CIl<- plot_fit-(1.96*(plot[[var_nr]]$se))
-  plot_CIu<- plot_fit+(1.96*(plot[[var_nr]]$se))
-  plot_data <-  data.frame(x=plot_x, fit=exp(plot_fit),CIl=exp(plot_CIl),CIu=exp(plot_CIu))
-}
 
 # Table for estimates and conf interval and Cohen's d
   ## Continuous outcomes:
@@ -38,6 +28,16 @@ GAM_plot_function_binarybis <- function(model, var_nr) {
   se <- sqrt(diag(Vb))
   d <- (abs(beta)/(sqrt(n)*se))
   my.ci <- data.frame(cbind(beta, lci = beta-1.96*se, uci = beta + 1.96*se, d))
+  }
+  
+  
+  estimates_cont_add_pval <- function(model, n)
+  {beta <- coef(model)
+  Vb <- vcov(model, unconditional = TRUE)
+  se <- sqrt(diag(Vb))
+  d <- abs(beta)/(sqrt(n)*se)
+  pvalue <- summary(model)$p.table[, "Pr(>|t|)"]
+  my.ci <- data.frame(cbind(beta, lci = beta-1.96*se, uci = beta + 1.96*se, d, pvalue))
   }
 
   ## Binary outcomes: 
@@ -58,7 +58,7 @@ getmode <- function(v) {
   uniqv[which.max(tabulate(match(v, uniqv)))]
 }
 
-# normalizing the values in m to the range [0, 1] using min-max scaling.
+# Normalizing the values in m to the range [0, 1] using min-max scaling.
 normalit<-function(m){
   (m - min(m))/(max(m)-min(m))
 }
