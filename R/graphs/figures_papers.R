45ad4a54ed231a2e0f78d54d0191a45172665d96
MathildeLV <mathilde.levu@iem.uzh.ch>
2024-01-07 19:29:34 +0100

    small modif

--- R/graphs/figures_papers.R
+++ R/graphs/figures_papers.R
@@ -1,67 +1,107 @@
 # Results, main figures
-## Figure 1, birthweight
-BW_figure1 <- ggarrange(plot_BW_time, plot_BW_SEP, plot_BW_nationality, plot_BW_language_region,
-                         labels = c("A", "B", "C", "D"),
-                         ncol = 2, nrow = 2) 
-BW_figure1
-ggsave("BW_figure1.pdf", BW_figure1, path = here("output/figures_paper"))
-ggsave("BW_figure1.jpg", BW_figure1, path = here("output/figures_paper"))
-
-## Figure 2, Preternbirth
-PTB_figure2 <- ggarrange(plot_PTB, ggplot_PTB_SEP, ggplot_PTB_nationality, ggplot_PTB_Language_region,
-                    labels = c("A", "B", "C", "D"),
-                    ncol = 2, nrow = 2)
-PTB_figure2
-ggsave("PTB_figure2.pdf", PTB_figure2, path = here("output/figures_paper"))
-ggsave("PTB_figure2.pdf", PTB_figure2, path = here("output/figures_paper"))
+## Figure 1.1, birthweight outcome model, main model and stratified by SEP/mat nationality/lang. region
+# UNADJUSTED FOR GESTATIONAL AGE
+BW_figure1_unadj_GA <- ggarrange(plot_BW_univ_unadj_GA,
+                                 plot_BW_sex,
+                                 plot_BW_SEP,
+                                 plot_BW_language_region,
+                                 plot_BW_nationality,
+                                 labels = c("A", "B", "C", "D", "E"),
+                                 ncol = 2, nrow = 3)
+BW_figure1_unadj_GA
 
 
+#DIRECTLY SAVE PDF 8*12 if 2*2 (2 col 2 rows)
+# Or save 17*8 (3 col 2 rows)
+# Or 12*12 (2 rows 3 columns)
+# ggsave("BW_figure1_unadj_GA.pdf", BW_figure1_unadj_GA, 
+#        path = here("output/figures_paper"))
+# ggsave("BW_figure1_unadj_GA.jpg", BW_figure1_unadj_GA, path = here("output/figures_paper"))
 
-## Figure 3, stillbirth (no stratification)
-ggsave("SB_figure3.pdf", plot_SB_time, path = here("output/figures_paper"))
-ggsave("SB_figure3.jpg", plot_SB_time, path = here("output/figures_paper"))
+
+
+## Figure 2, Preterm birth outcome model, main model and stratified by SEP/mat nationality/lang. region
+## UNADJUSTED FOR BW
+PTB_figure2_unadj_GA_sex <- ggarrange(plot_PTB_univ_unadj_BW,
+                                 plot_PTB_sex,
+                                 ggplot_PTB_SEP2,
+                                 ggplot_PTB_Language_region2,
+                                 ggplot_PTB_nationality2,
+                                 labels = c("A", "B", "C", "D", "E"),
+                                 ncol = 2, nrow = 3)
+PTB_figure2_unadj_GA_sex
+#DIRECTLY SAVE PDF 8*12
+# ggsave("PTB_figure2_unadj_for_BW.pdf", PTB_figure2_2, path = here("output/figures_paper"))
+# ggsave("PTB_figure2_unadj_for_BW.jpg", PTB_figure2_2, path = here("output/figures_paper"))
+
+
+
+## Figure 3, stillbirth model (no stratification)
+plot_SB_univ
+#DIRECTLY SAVE PDF 8*12
+# ggsave("SB_figure3.pdf", plot_SB_univ, path = here("output/figures_paper"))
 
 # Appendix
-  ## S1 : smoothed variables, BW model
-BW_supplfig_1 <- ggarrange(plot_BW_GA, plot_BW_mat_age, plot_BW_month, plot_BW_ssep, plot_BW_altitude,
-                          labels = c("A", "B", "C", "D", "E"),
-                          ncol = 3, nrow = 2) 
-BW_supplfig_1
-ggsave("BW_supplfig_1.pdf", BW_supplfig_1, path = here("output/figures_paper"))
-ggsave("BW_supplfig_1.jpg", BW_supplfig_1, path = here("output/figures_paper"))
-
-  ## S2 : smoothed variables, PTB model
-PTB_supplfig_2 <- ggarrange(plot_PTB_BW, plot_PTB_mat_age, plot_PTB_seasonality, plot_PTB_SSEP, plot_PTB_altitude,
-                          labels = c("A", "B", "C", "D", "E"),
-                          ncol = 3, nrow = 2) 
-PTB_supplfig_2
-ggsave("PTB_supplfig_2.pdf", PTB_supplfig_2, path = here("output/figures_paper"))
-ggsave("PTB_supplfig_2.jpg", PTB_supplfig_2, path = here("output/figures_paper"))
-
-  ## S3 : smoothed variables, stillbirth model
-SB_supplfig_3 <- ggarrange(plot_SB_GA,  plot_SB_mat_age,  plot_SB_month,  plot_SB_SSEP, plot_SB_altitude,
-                           labels = c("A", "B", "C", "D", "E"),
-                           ncol = 3, nrow = 2) 
-SB_supplfig_3
-ggsave("SB_supplfig_3.pdf", SB_supplfig_3, path = here("output/figures_paper"))
-ggsave("SB_supplfig_3.jpg", SB_supplfig_3, path = here("output/figures_paper"))
+  ## S1 : smoothed variables, BW main model
 
 
+## S1.1 : smoothed variables, BW main model WITHOUT ADJUSTING for GA 
+BW_supplfig_1_unadj_GA <- ggarrange(plot_BW_main_mat_age, plot_BW_main_month,
+                                    labels = c("A", "B"),
+                                    ncol = 2, nrow = 1) 
+BW_supplfig_1_unadj_GA
+# directly save 9*4.5
+# ggsave("BW_supplfig_1_unadj_GA.pdf", BW_supplfig_1_unadj_GA, path = here("output/figures_paper"))
+# ggsave("BW_supplfig_1_unadj_GA.jpg", BW_supplfig_1_unadj_GA, path = here("output/figures_paper"))
+
+## S2 : smoothed variables, PTB main model WITHOUT ADJUSTING for BW
+PTB_supplfig_2_2 <- ggarrange(plot_PTB_main_mat_age, plot_PTB_main_seasonality,
+                              labels = c("A", "B"),
+                              ncol = 2, nrow = 1) 
+PTB_supplfig_2_2
+# directly save 9*4.5
+# ggsave("PTB_supplfig_2_unadj_for_BW.pdf", PTB_supplfig_2_2, path = here("output/figures_paper"))
+# ggsave("PTB_supplfig_2_unadj_for_BW.jpg", PTB_supplfig_2_2, path = here("output/figures_paper"))
 
 
+## S3 : smoothed variables, stillbirth main model
+# with mat nationality category 1 and
+# WITHOUT ADJUSTING for GESTATIONAL AGE
+SB_supplfig_3_simple_unadj_GA <- ggarrange(plot_SB_main_mat_age, 
+                                           plot_SB_main_month,
+                                           labels = c("A", "B"),
+                                           ncol = 2, nrow = 1) 
+SB_supplfig_3_simple_unadj_GA
+# directly save 9*4.5
+# ggsave("SB_supplfig_3_simple_unadj_GA.pdf", SB_supplfig_3_simple_unadj_GA, path = here("output/figures_paper"))
+# ggsave("SB_supplfig_3_simple_unadj_GA.jpg", SB_supplfig_3_simple_unadj_GA, path = here("output/figures_paper"))
 
 
+## Models with flu instead of Great Recession
+BW_supplfig_4_flu <- ggarrange(plot_BW_mat_age_flu, plot_BW_month_flu,
+                                    labels = c("A", "B"),
+                                    ncol = 2, nrow = 1) 
+BW_supplfig_4_flu
 
-# trying out plots
-(plot_PTB | ggplot_PTB_SEP) + (ggplot_PTB_nationality |ggplot_PTB_Language_region)  
-test <- (plot_PTB | ggplot_PTB_SEP + ggplot_PTB_nationality +ggplot_PTB_Language_region  )+ plot_layout(nrow = 2, byrow = FALSE)
-ggsave("test.pdf", test, path = here("output/stratification/graphs"))
-test <- (plot_PTB | ggplot_PTB_SEP + ggplot_PTB_nationality +ggplot_PTB_Language_region  )+ plot_layout(nrow = 2, byrow = FALSE)
-test
-test <- (plot_PTB + ggplot_PTB_SEP) | (ggplot_PTB_nationality + ggplot_PTB_Language_region  )+ plot_layout(nrow = 2, byrow = FALSE)
+PTB_supplfig_5_flu <- ggarrange(plot_PTB_flu_mat_age, plot_PTB_flu_seasonality,
+                              labels = c("A", "B"),
+                              ncol = 2, nrow = 1) 
+PTB_supplfig_5_flu
 
-(plot_PTB | ggplot_PTB_SEP) + (ggplot_PTB_nationality | ggplot_PTB_Language_region  ) + plot_layout(nrow = 2, byrow = FALSE)
+SB_supplfig_6flu <- ggarrange(plot_SB_flu_mat_age, 
+                              plot_SB_flu_month,
+                              labels = c("A", "B"),
+                              ncol = 2, nrow = 1) 
+SB_supplfig_6flu
 
-# 2*2 as square
-plot_PTB + ggplot_PTB_SEP + ggplot_PTB_nationality+ ggplot_PTB_Language_region
-(plot_PTB / (ggplot_PTB_SEP + ggplot_PTB_nationality+ ggplot_PTB_Language_region)+ plot_layout(nrow = 2, byrow = FALSE))
+
+## Sensitivity analyses: first parities only
+BW_supplfig_7_prim <- ggarrange(plot_BW_prim_mat_age, plot_BW_prim_month,
+                               labels = c("A", "B"),
+                               ncol = 2, nrow = 1) 
+BW_supplfig_7_prim
+
+PTB_supplfig_8_prim <- ggarrange(plot_PTB_prim_mat_age, plot_PTB_prim_seasonality,
+                                labels = c("A", "B"),
+                                ncol = 2, nrow = 1) 
+PTB_supplfig_8_prim
