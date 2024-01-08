7cdb85d9a3a3b609fc1431a4618c538add753fdd
MathildeLV <mathilde.levu@iem.uzh.ch>
2024-01-08 09:48:19 +0100

    updating master file

--- master.R
+++ master.R
@@ -1,4 +1,4 @@
-.libPaths(c("H:/Documents/R/win-library/4.1","C:/Program Files/R/R-4.3.1/library"))
+.libPaths(c("H:/Documents/R/win-library/4.1","C:/Program Files/R/R-4.3.2/library"))
 
 #packages
 library(dplyr)
@@ -22,7 +22,7 @@ library(rmdformats)
 library(oddsratio)
 library(nnet)
 library(network)
- library(lpSolveAPI)
+ # library(lpSolveAPI)
  library(RDS)
 # library(quantreg)
 library(scales)
@@ -30,12 +30,12 @@ library(ggpubr)
 library(egg)
 library(gridExtra)
 library(patchwork)
-library(webshot2) #to take screenshots of gt tables and save them as images
 library(effects)
 library(broom) 
 library(conflicted)
 library(usdm) # for VIF multicollinearity factor
- 
+library(LaplacesDemon)
+
 conflict_prefer("filter", "dplyr")
 conflict_prefer("rename", "dplyr")
 conflict_prefer("select", "dplyr")
@@ -47,26 +47,21 @@ conflict_prefer("ggarrange", "egg")
 
 
 #  data from 2007 only
-# source("R/bevn_from_2007.R")
-
 bevn <- read.csv2(here("data", "bevn2007.csv"), sep = ",")
+# add years 2021 and 2022
 bevn_2021 <- read.csv2(here("data", "NAISS21.csv"))
 bevn_2022 <- read.csv2(here("data", "NAISS22.2.csv"),  fileEncoding = 'UTF-8-BOM')
+
 #ecological variables data
-anth <- read.csv2(here("data", "Anthropo.csv"))
-gem <- read.csv2(here("data", "Gemeinde.csv"))
-pop <- read.csv2(here("data", "swiss_population_per_year.csv"),  fileEncoding = 'UTF-8-BOM')
+# anth <- read.csv2(here("data", "Anthropo.csv")) # to add MS Region number
+gem <- read.csv2(here("data", "Gemeinde.csv")) # to add municipality number, SSEP, Language region, Urbanity, altitude
+
+# Whole Swiss population, permanent resident or not, population 2007 to 2022
+pop <- read.csv2(here("data", "permanent_and_non_permanent_population_2010_2022.csv"),  fileEncoding = 'UTF-8-BOM')
+
 # covid hospitalization numbers per week
 data.covid.org <- read.csv2(here("data", "COVID19Hosp_geoRegion_w.csv"))
 
-
-#loading R codes
-source("R/functions.R")
-source("R/merging.R") #to run separately
-source("R/data_preparation.R") #to run separately
-source("R/covid_monthly_exposure_hospitalisations.R")  # create variable for exposure to COVID (sum of hospitalisation cases during pregnancy)
-source("R/dummyvariables_crisis.R") #variables for exposure to heatwave, great recession, and 2009 flu pandemic
-source("R/Inclusions_fast.R")
 
 #graph parameters
 lwdline <- 1.2
@@ -76,54 +71,54 @@ size_legend <- 10
 size_legend_title <- 12
 axis.title.x.position <- element_text(margin = margin(t =12))
 
+#loading R codes
+source("R/functions.R")
+source("R/merging.R") #to run separately
+source("R/data_preparation.R") #to run separately
+source("R/covid_monthly_exposure_hospitalisations.R")  # create variable for exposure to COVID (sum of hospitalisation cases during pregnancy)
+source("R/dummyvariables_crisis.R") #variables for exposure to heatwave, great recession, and 2009 flu pandemic
+source("R/Inclusions_fast.R")
+
+
 # Render Rmd files in html
 #Univ/descriptive and multiv analysis
-render("R/descript_and_univ_analysis.Rmd", output_file = paste0("../output/",today(),"descript_and_univ_analysis.html"))
-render("R/multivariate_analysis.Rmd", output_file = paste0("../output/",today(),"multivariate_models_lm_glm.html"))
-## Gam models
-render("R/gam_birthweight.Rmd", output_file = paste0("../output/",today(),"gam_birthweight.html"))
-render("R/gam_stillbirth.Rmd", output_file = paste0("../output/",today(),"gam_stillbirth.html"))
-render("R/gam_gest_age.Rmd", output_file = paste0("../output/",today(),"gam_gest_age.html"))
-render("R/gam_LBW_normal_macro.Rmd", output_file = paste0("../output/",today(),"gam_LBW_normal_macro.html"))
-render("R/GAM_summary.Rmd", output_file = paste0("../output/",today(),"GAM_summary.html"))
-render("R/GAM_summary_meeting_20221111.Rmd", output_file = paste0("../output/",today(),"GAM_summary_meeting_20221111.html"))
-render("R/GAM_Summary_all_var_different_outcomes.Rmd", output_file = paste0("../output/",today(),"GAM_summary_all_var_different_outcomes.html"))
+# render("R/descript_and_univ_analysis.Rmd", output_file = paste0("../output/",today(),"descript_and_univ_analysis.html"))
+# render("R/multivariate_analysis.Rmd", output_file = paste0("../output/",today(),"multivariate_models_lm_glm.html"))
 
-### Additional GAM: stratification, interaction, etc
-  ## BW outcome
-  render("R/stratified_models/BW_GAM_stratified_by_maternal_nationality_SEP.Rmd", output_file = paste0("../../output/stratification/",today(),"BW_GAM_stratif_by_mat_nationality_SEP.html"))
-  render("R/stratified_models/BW_GAM_stratified_by_SSEP_Category.Rmd", output_file = paste0("../../output/stratification/",today(),"BW_GAM_stratif_by_SSEP_Category.html"))
-  render("R/stratified_models/BW_GAM_stratified_by_language_region.Rmd", output_file = paste0("../../output/stratification/",today(),"BW_GAM_stratif_by_language_region.html"))
-  render("R/stratified_models/BW_GAM_interaction_SSEP_X_language_region.Rmd", output_file = paste0("../../output/stratification/",today(),"BW_GAM_interaction_SSEP_X_language_reg.html"))
-  render("R/GAM_zoom_in_heatwaves.Rmd", output_file = paste0("../output/",today(),"GAM_zoom_in_heatwaves.html"))
+# Methods: table 1 for exposure to the crises through time
+render("R/crises_exposure_variables_simple_tb_and_association_with_birth_outcomes.Rmd", output_file = paste0("../output/",today(),"crises_exposure_variables_through time.html"))
 
-  ## PTB outcome
-  render("R/stratified_models/PTB_GAM_stratified_by_SSEP_Category.Rmd", output_file = paste0("../../output/stratification/",today(),"PTB_GAM_stratif_by_SSEP_Category.html"))
-  render("R/stratified_models/PTB_GAM_stratified_by_maternal_nationality.Rmd", output_file = paste0("../../output/stratification/",today(),"PTB_GAM_stratif_by_maternal_nationality.html"))
-  render("R/stratified_models/PTB_GAM_stratified_by_Language_Region.Rmd", output_file = paste0("../../output/stratification/",today(),"PTB_GAM_stratif_by_Language_Region.html"))
-  render("R/stratified_models/PTB_GAM_stratified_by_maternal_age.Rmd", output_file = paste0("../../output/stratification/",today(),"PTB_GAM_stratified_by_mat_age.html"))
-  render("R/stratified_models/Basel_PTB_stratified_by_maternal_age.Rmd", output_file = paste0("../../output/stratification/",today(),"PTB_GAM_Basel_stratified_by_mat_age.html"))
-  
-  ## SB outcome
-  render("R/stratified_models/SB_GAM_stratified_by_SSEP_Category.Rmd", output_file = paste0("../../output/stratification/",today(),"SB_GAM_stratif_by_SSEP_Category.html"))
-  render("R/stratified_models/SB_GAM_stratified_by_Language_Region.Rmd", output_file = paste0("../../output/stratification/",today(),"SB_GAM_stratif_by_Language_Region.html"))
-  render("R/stratified_models/SB_GAM_stratified_by_Maternal_Nationality.Rmd", output_file = paste0("../../output/stratification/",today(),"SB_GAM_stratif_by_Maternal_Nationality.html"))
-  
+## GAM models
+### univariable models
+render("R/GAM_models_BW_PTB_SB_univariable_with_time_only.Rmd", output_file = paste0("../output/",today(),"GAM_models_BW_PTB_SB_univariable_with_time_only.html"))
+### multivariable models
+render("R/GAM_main_multivariate_models.Rmd", output_file = paste0("../output/",today(),"GAM_main_multivariate_models.html"))
+### stratified models
+render("R/stratified_models/graphs_without_lines_BW_GAM_graphs_stratification_nationality_language_SEP_sex.Rmd", output_file = paste0("../output/stratification",today(),"BW_GAM_graphs_stratification_nationality_language_SEP_sex.html"))
+render("R/stratified_models/graphs_without_lines_PTB_GAM_graphs_stratification_nationality_language_SEP_sex.Rmd", output_file = paste0("../output/stratification",today(),"PTV_GAM_graphs_stratification_nationality_language_SEP_sex.html"))
+render("R/stratified_models/graphs_without_lines_SB_GAM_graphs_stratification_nationality_language_SEP_sex.Rmd", output_file = paste0("../output/stratification",today(),"SB_GAM_graphs_stratification_nationality_language_SEP_sex.html"))
+
+
+
+### Additional GAMs: stratification, interaction, etc
   ## sensitivity analysis: primiparous women only
-  render("R/primiparous_women_BW_PTB_stratification.Rmd", output_file = paste0("../output/primiparous_women/",today(),"BW_PTB_whole_models_and_stratif_by_Language_SEP_nationality_primiparous.html"))
+  render("R/GAM_BW_PTB_first_parities.Rmd", output_file = paste0("../output/",today(),"BW_PTB_primiparous.html"))
+  ## sensitivity analysis: flu exposure instead of Great Recession exposure
+  render("R/GAM_models_flu_pandemic.Rmd", output_file = paste0("../output/",today(),"GAM_models_flu_pandemic.html"))
   
-  ## trimester effect
+
+## trimester effect
   render("R/trimester_effect_crises/COVID_trimester_effect.Rmd", output_file = paste0("../../output/trimester_effect_crisis",today(),"COVID_trimester_effect.html"))
   render("R/trimester_effect_crises/great_recession_trimester_effect.Rmd", output_file = paste0("../../output/trimester_effect_crisis",today(),"great_recession_trimester_effect.html"))
   render("R/trimester_effect_crises/heatwave_trimester_effect.Rmd", output_file = paste0("../../output/trimester_effect_crisis",today(),"heatwave_trimester_effect.html"))
   source("R/trimester_effect_crises/table_trimester_effect.R")
+ 
+###Tables for describing charactertics of the studied populations
+  # including birth rate per year 
+  render("R/tables/whole_tb_charact_population.Rmd", output_file = paste0("../output/",today(),"whole_tb_charact_population.html"))
+  source("R/tables/whole_tb_charact_population_inverting.R")
   
-## Tables and graphs
-render("R/whole_tb_charact_population.Rmd", output_file = paste0("../output/",today(),"whole_tb_charact_population.html"))
-render("R/graphs/graphics.Rmd", output_file = paste0("../../output/graphs/",today(),"graphics.html"))
-source("R/whole_tb_charact_population_inverting.R")
-render("R/stratified_models/graphs/BW_GAM_graphs_stratification_nationality_language_SEP.Rmd", output_file = paste0("../../../output/stratification/graphs/",today(),"BW_GAM_stratification_nationality_language_SEP.html"))
-render("R/stratified_models/graphs/graphs_without_lines_BW_GAM_graphs_stratification_nationality_language_SEP.Rmd", output_file = paste0("../../../output/stratification/graphs/",today(),"graphs_without_lines_BW_GAM_stratification_nationality_language_SEP.html"))
-render("R/stratified_models/graphs/PTB_GAM_graphs_stratification_nationality_language_SEP.Rmd", output_file = paste0("../../../output/stratification/graphs/",today(),"PTB_GAM_stratification_nationality_language_SEP.html"))
-render("R/stratified_models/graphs/graphs_without_lines_PTB_GAM_graphs_stratification_nationality_language_SEP.Rmd", output_file = paste0("../../../output/stratification/graphs/",today(),"graphs_without_lines_PTB_GAM_stratification_nationality_language_SEP.html"))
-render("R/stratified_models/graphs/SB_GAM_graphs_stratification_nationality_language_SEP.Rmd", output_file = paste0("../../../output/stratification/graphs/",today(),"SB_GAM_stratification_nationality_language_SEP.html"))
+### Figures
+  source("R/graphs/figures_papers.R")
+  render("R/graphs/covariates_through_time_and_number_births_per_covariate.Rmd", output_file = paste0("../output/figures_paper",today(),"birthrate_through_time.html"))
+  
